703fb45361f7ed0d86ada008ae4466bb6c8395f6
Sam Bowcut <sbowcut@gmail.com>
2021-07-29 09:58:04 -0600

    functions

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -1,42 +1,33 @@
 USE employees;
 
-SELECT *
+SELECT CONCAT(first_name, ' ', last_name)
 FROM employees
-WHERE first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY last_name DESC, first_name DESC;
-
-SELECT *
-FROM employees
-WHERE last_name LIKE 'E%';
-
-SELECT *
-FROM employees
-WHERE last_name LIKE '%q%';
+WHERE last_name LIKE 'E%' OR last_name LIKE '%E';
 
 SELECT *
 FROM employees.employees
-WHERE (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya')
-  AND gender = 'm';
+WHERE DAY(birth_date) = 25
+AND MONTH(birth_date) = 12;
 
 SELECT *
-FROM employees
-WHERE last_name LIKE 'E%'
-   OR last_name LIKE '%e' ORDER BY emp_no DESC;
+FROM employees.employees
+WHERE DAY(birth_date) = 25
+AND MONTH(birth_date) = 12
+AND YEAR(hire_date) >= 1990
+AND YEAR(hire_date) < 2000;
 
 SELECT *
-FROM employees
-WHERE last_name LIKE 'E%'
-  AND last_name LIKE '%e' ORDER BY emp_no DESC;
+FROM employees.employees
+WHERE DAY(birth_date) = 25
+  AND MONTH(birth_date) = 12
+  AND year(hire_date) >= 1990
+  AND year(hire_date) < 2000
+ORDER BY birth_date, hire_date DESC;
 
-SELECT *
-FROM employees
-WHERE last_name LIKE 'E%e' ORDER BY emp_no DESC;
-
-SELECT *
-FROM employees
-WHERE last_name LIKE '%q%'
-  AND last_name NOT LIKE 'qu%';
-
-SELECT *
-FROM employees
-WHERE last_name LIKE '%q%'
-  AND last_name NOT LIKE '%qu%';
+SELECT *, DATEDIFF(CURDATE(), hire_date)
+FROM employees.employees
+WHERE DAY(birth_date) = 25
+  AND MONTH(birth_date) = 12
+  AND YEAR(hire_date) >= 1990
+  AND YEAR(hire_date) < 2000
+ORDER BY birth_date, hire_date DESC;
--- limit_exercises.sql
+++ limit_exercises.sql
@@ -14,4 +14,4 @@ LIMIT 5;
 SELECT emp_no
 FROM employees.salaries
 ORDER BY salary DESC
-LIMIT 4 OFFSET 46;
+LIMIT 5 OFFSET 45;
