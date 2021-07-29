f01ac8f10b1a60475ea35cb0968cb3f40c158139
Sam Bowcut <sbowcut@gmail.com>
2021-07-29 10:21:06 -0600

    functions

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -5,29 +5,28 @@ FROM employees
 WHERE last_name LIKE 'E%' OR last_name LIKE '%E';
 
 SELECT *
-FROM employees.employees
+FROM employees
 WHERE DAY(birth_date) = 25
 AND MONTH(birth_date) = 12;
 
 SELECT *
-FROM employees.employees
+FROM employees
 WHERE DAY(birth_date) = 25
 AND MONTH(birth_date) = 12
 AND YEAR(hire_date) >= 1990
 AND YEAR(hire_date) < 2000;
 
 SELECT *
-FROM employees.employees
+FROM employees
 WHERE DAY(birth_date) = 25
-  AND MONTH(birth_date) = 12
-  AND year(hire_date) >= 1990
+AND MONTH(birth_date) = 12
+AND year(hire_date) >= 1990
   AND year(hire_date) < 2000
 ORDER BY birth_date, hire_date DESC;
 
-SELECT *, DATEDIFF(CURDATE(), hire_date)
-FROM employees.employees
+SELECT *, DATEDIFF(CURDATE(), hire_date) AS DATEDIFF
+FROM employees
 WHERE DAY(birth_date) = 25
   AND MONTH(birth_date) = 12
   AND YEAR(hire_date) >= 1990
-  AND YEAR(hire_date) < 2000
-ORDER BY birth_date, hire_date DESC;
+  AND YEAR(hire_date) < 2000;
